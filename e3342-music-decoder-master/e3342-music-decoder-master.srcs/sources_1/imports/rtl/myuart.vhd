c8de45cd694c0abae4fff8e7bdacd80b8608df0d
Asad <99832552+AsadNaveed1@users.noreply.github.com>
2023-11-29 02:21:16 +0800

    Our own myuart

--- e3342-music-decoder-master/e3342-music-decoder-master.srcs/sources_1/imports/rtl/myuart.vhd
+++ e3342-music-decoder-master/e3342-music-decoder-master.srcs/sources_1/imports/rtl/myuart.vhd
@@ -1,93 +1,105 @@
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.NUMERIC_STD.ALL;
 
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
--- library UNISIM;
--- use UNISIM.VComponents.all;
 
-entity myuart is
-    port (
-           din  : in std_logic_vector (7 downto 0);
-           busy : out std_logic;
-           wen  : in std_logic;
-           sout : out std_logic;
-           clr  : in std_logic;
-           clk  : in std_logic);
-end myuart;
+ENTITY myuart IS
+	PORT (
+		din : IN STD_LOGIC_VECTOR (7 DOWNTO 0);
+		busy : OUT STD_LOGIC;
+		wen : IN STD_LOGIC;
+		sout : OUT STD_LOGIC;
+		clr : IN STD_LOGIC;
+		clk : IN STD_LOGIC
+	);
+END myuart;
 
-architecture rtl of myuart is
-    type state_type is (St_IDLE, St_BUSY);
-    signal state, next_state : state_type := St_IDLE;
+ARCHITECTURE rtl OF myuart IS
+	TYPE state_type IS (idle, start, d0, d1, d2, d3, d4, d5, d6, d7, stop);
+	SIGNAL state : state_type := idle;
+	SIGNAL cnt : INTEGER RANGE 0 TO 9 := 0;
+BEGIN
+	statechg : PROCESS (clr, clk, cnt)
+	BEGIN
+		IF clr = '1' THEN
+			state <= idle;
+		ELSIF rising_edge(clk) THEN
+			IF cnt < 9 THEN
+				cnt <= cnt + 1;
+			ELSE
+				cnt <= 0;
+			END IF;
+			CASE(state) IS
+				WHEN idle => 
+					IF wen = '1' THEN
+		                  		sout <= '1';
+				          	busy <= '0';
+				          	state <= start;
+				          			         
+					END IF;
+				WHEN start => 
+					IF cnt = 9 THEN
+						sout <= '0';
+				          	busy <= '1';
+				          	state <= d0;
+					END IF;
+				WHEN d0 => 
+					IF cnt = 9 THEN
+					      	sout <= din(0);
+				          	busy <= '1';
+				          	state <= d1;
+					END IF;
+				WHEN d1 => 
+					IF cnt = 9 THEN
+						sout <= din(1);
+				          	busy <= '1';
+				          	state <= d2;
+					END IF;
+				WHEN d2 => 
+					IF cnt = 9 THEN
+						sout <= din(2);
+				          	busy <= '1';
+				          	state <= d3;
+					END IF;
+				WHEN d3 => 
+					IF cnt = 9 THEN
+						sout <= din(3);
+				          	busy <= '1';
+				          	state <= d4;
+					END IF;
+				WHEN d4 => 
+					IF cnt = 9 THEN
+					   	sout <= din(4);
+				          	busy <= '1';
+						state <= d5;
+					END IF;
+				WHEN d5 => 
+					IF cnt = 9 THEN
+					   	sout <= din(5);
+				          	busy <= '1';
+						state <= d6;
+					END IF;
+				WHEN d6 => 
+					IF cnt = 9 THEN
+						sout <= din(6);
+				          	busy <= '1';
+						state <= d7;
+					END IF;
+				WHEN d7 => 
+					IF cnt = 9 THEN
+					   	sout <= din(7);
+				          	busy <= '1';
+						state <= stop;
+					END IF;
+				WHEN stop => 
+					IF cnt = 9 THEN
+					   	 sout <= '1';
+				          	 busy <= '1';
+						 state <= idle;
+					END IF;
+ 
+			END CASE;
+		END IF;
+	END PROCESS;
 
-    signal wen_buffer : std_logic;
-    signal din_buffer : std_logic_vector (8 downto 0);
-    signal bit_seq, baud_timer : unsigned (3 downto 0) := x"0";
-begin
-    sync_process: process (clk, clr)
-    begin
-        if clr = '1' then
-            state <= St_IDLE;
-        elsif rising_edge(clk) then
-            wen_buffer <= wen;
-            state <= next_state;
-
-            if state = St_IDLE then
-                bit_seq <= x"0";
-                baud_timer <= x"0";
-            else
-                if baud_timer = x"9" then
-                    baud_timer <= x"0";
-                    if bit_seq = x"9" then
-                        bit_seq <= x"0";
-                    else
-                        bit_seq <= bit_seq + 1;
-                    end if;
-                else
-                    baud_timer <= baud_timer + 1;
-                end if;
-            end if;
-        end if;
-    end process;
-
-    state_logic: process (wen, baud_timer)
-    begin
-        next_state <= state;
-        case(state) is
-            when St_IDLE =>
-                if (wen = '1' and wen_buffer = '0') then
-                    din_buffer <= din & "0";
-                    next_state <= St_BUSY;
-                end if;
-            when St_BUSY =>
-                if bit_seq = x"9" and baud_timer = x"9" then
-                    next_state <= St_IDLE;
-                end if;
-            when others =>
-                next_state <= St_IDLE;
-        end case;
-    end process;
-
-    output_logic: process (state, bit_seq)
-    begin
-        case(state) is
-            when St_IDLE =>
-                sout <= '1';
-                busy <= '0';
-            when St_BUSY =>
-                busy <= '1';
-                case(bit_seq) is
-                    when x"0" =>
-                        sout <= '0';
-                    when x"9" =>
-                        sout <= '1';
-                    when others =>
-                        sout <= din_buffer(to_integer(bit_seq));
-                end case;
-            when others =>
-                null;
-        end case;
-    end process;
-end rtl;
-
+END rtl;
